a7eebf40414b9a34d3fd2e2f87b8a70ce0e2f07b
tioibiy <159704005+TobyDaddy@users.noreply.github.com>
2024-03-30 16:11:21 +0800

    new file:   app.py 	new file:   requirements.txt 	new file:   templates/index.html

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+from PIL import Image
+import os
+import io
+import base64
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://fwwwfkjoco:7O48FKA30IRL0L68$@bamaster-server.postgres.database.azure.com/bamaster_database'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+db = SQLAlchemy(app)
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.LargeBinary)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        image = Image(data=file.read())
+        db.session.add(image)
+        db.session.commit()
+        return redirect(url_for('uploaded_file', id=image.id))
+
+    return render_template('index.html')
+
+@app.route('/uploads/<id>')
+def uploaded_file(id):
+    image = Image.query.get(id)
+    return base64.b64encode(image.data)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
