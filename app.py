5f974933943fef1386f32478c382ec7edecb35e6
tioibiy <159704005+TobyDaddy@users.noreply.github.com>
2024-03-30 18:21:38 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def connect_db():
     try:
         conn = psycopg2.connect(
             user="fwwwfkjoco",
-            password="your_password",
+            password="7O48FKA30IRL0L68$",
             host="bamaster-server.postgres.database.azure.com",
             port="5432",
             database="bamaster-database"
